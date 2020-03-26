cf673d5d58f0f83df133874ea106a1df8897f09e
unknown <jesssepinkman97@gmail.com>
2020-03-26 21:35:26 +0700

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, friend! I am a basic React component.
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ProductList />,
+  document.getElementById('content')
+);
+class ProductList extends React.Component {
+  render(){
+    return(
+      <div className= "ui unstackable items">
+        Hello mom !!! 
+      </div>
+    )
+  }
+}
